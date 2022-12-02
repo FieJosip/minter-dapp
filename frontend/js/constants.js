0c321dc9f92d3f123af3cf8566ee140ad82c44aa
fiejosip <fiejosip@gmail.com>
2022-12-02 02:54:03 +0100

    test1

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x544721AacD6B57e9211Fbd0513df85328A20221F"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
